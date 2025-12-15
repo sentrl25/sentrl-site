d33472f4f564c39481f798e6b534581b18e84fb2
sentrl25 <matt@sentrl.com.au>
2025-12-16 05:35:02 +0930

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
+import { Layout } from './Layout';
 import { Home } from './pages/Home';
 import { Platform } from './pages/Platform';
 import { Solutions } from './pages/Solutions';
